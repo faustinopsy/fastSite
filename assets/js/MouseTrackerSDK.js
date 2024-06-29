359f24b92785778d82bedb0bffd9be89f75dbde6
faustinopsy <rodrigohipnose@gmail.com>
2024-06-28 21:57:11 -0300

    ok

--- assets/js/MouseTrackerSDK.js
+++ assets/js/MouseTrackerSDK.js
@@ -3,21 +3,25 @@ class MouseTrackerSDK {
         this.options = options;
         this.movimentoMouser = [];
         this.rede = null;
-        this.downlink  = null;
+        this.downlink = null;
         this.referer = document.referrer;
+        this.sessionId = this.generateSessionId();
+        this.lastInteraction = Date.now();
         this.init();
     }
 
     init() {
-        if(navigator.connection){
-            navigator.connection.addEventListener('change', this.statusRede());
+        if (navigator.connection) {
+            navigator.connection.addEventListener('change', this.statusRede.bind(this));
         }
         document.addEventListener('visibilitychange', () => {
             if (document.visibilityState === 'hidden') {
                 this.enviarSevidor();
             }
         });
-        window.addEventListener('beforeunload', this.enviarSevidor());
+        window.addEventListener('beforeunload', () => {
+            this.enviarSevidor();
+        });
 
         const touchArea = document.body;
 
@@ -27,20 +31,22 @@ class MouseTrackerSDK {
         touchArea.addEventListener('touchcancel', this.rastrearMovimento.bind(this));
 
         document.addEventListener('mousemove', this.rastrearMovimento.bind(this));
-         
     }
+
+    generateSessionId() {
+        return '_' + Math.random().toString(36).substr(2, 9);
+    }
+
     statusRede() {
-        if(navigator.connection){
+        if (navigator.connection) {
             this.rede = navigator.connection.effectiveType;
-            this.downlink  = navigator.connection.downlink
-        }else{
-            this.rede ='ff'
-            this.downlink =0
+            this.downlink = navigator.connection.downlink;
+        } else {
+            this.rede = 'ff';
+            this.downlink = 0;
         }
     }
-    isMobile() {
-        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1) || window.innerWidth <= 600;
-    }
+
     getOriginType() {
         const referer = this.referer.toLowerCase();
         if (referer.includes('google.com')) {
@@ -51,18 +57,19 @@ class MouseTrackerSDK {
             return 'Instagram';
         } else if (referer.includes('twitter.com')) {
             return 'Twitter';
-        }else if (referer.includes('br.linkedin.com')) {
-            return 'linkedin';
-        } 
-        else if (referer.includes('youtube.com')) {
-            return 'youtube';
+        } else if (referer.includes('br.linkedin.com')) {
+            return 'LinkedIn';
+        } else if (referer.includes('youtube.com')) {
+            return 'YouTube';
         } else {
             return 'Outra';
         }
     }
-    
+
     rastrearMovimento(event) {
-        this.statusRede()
+        this.statusRede();
+        this.lastInteraction = Date.now();
+
         let dadosMovimento = null;
 
         if (event.type.startsWith('touch')) {
@@ -71,44 +78,52 @@ class MouseTrackerSDK {
                 x: touch.clientX,
                 y: touch.clientY,
                 timestamp: Date.now(),
-                mobile: this.isMobile(),
+                width: screen.width,
+                height: screen.height,
                 url: location.href,
                 rede: this.rede,
                 velocidade: this.downlink,
-                referer: this.referer, 
-                origin: this.getOriginType()
+                referer: this.referer,
+                origin: this.getOriginType(),
+                sessionId: this.sessionId
             };
         } else {
             dadosMovimento = {
                 x: event.pageX,
                 y: event.pageY,
                 timestamp: Date.now(),
-                mobile: this.isMobile(),
+                width: screen.width,
+                height: screen.height,
                 url: location.href,
                 rede: this.rede,
                 velocidade: this.downlink,
-                referer: this.referer, 
-                origin: this.getOriginType()
+                referer: this.referer,
+                origin: this.getOriginType(),
+                sessionId: this.sessionId
             };
         }
-        //console.log(this.movimentoMouser);
-        const url = location.hash;
-        if (!this.movimentoMouser[url]) {
-            this.movimentoMouser[url] = [];
-        }
-        this.movimentoMouser.push(dadosMovimento);
-        // if (this.movimentoMouser.length >= 15 && this.options.endpoint) {
-        //     this.enviarSevidor();
-        // }
 
-        if (this.movimentoMouser.length > this.options.bufferSize) {
-            this.movimentoMouser.shift();
+        
+
+        if (this.movimentoMouser.length <= this.options.bufferSize) {
+            this.movimentoMouser.push(dadosMovimento);
+            //this.movimentoMouser.shift();
         }
+
+        localStorage.setItem('movimentoMouser', JSON.stringify(dadosMovimento));
     }
 
     enviarSevidor() {
         if (this.movimentoMouser.length === 0) {
             return;
+        }
+
+        const sessionDuration = (Date.now() - this.lastInteraction) / 1000; 
+        const movimentosArmazenados = JSON.parse(localStorage.getItem('movimentoMouser'));
+        const dadosFinais = movimentosArmazenados;
+        
+        if (dadosFinais) {
+            this.movimentoMouser.push(dadosFinais);
         }
 
         const dataToSend = {
@@ -131,8 +146,8 @@ class MouseTrackerSDK {
             return response.json();
         })
         .then(data => {
-            //console.log('Dados enviados:', data);
             this.movimentoMouser = [];
+            localStorage.removeItem('movimentoMouser');
         })
         .catch(error => {
             console.error('Erro ao enviar dados:', error);
@@ -144,8 +159,6 @@ const options = {
     endpoint: '/backAnalitics/api.php',
     apiToken: '123',
     bufferSize: 10,
-    siteKey: 'your-site-key'
+    siteKey: '000000009'
 };
 const mouser = new MouseTrackerSDK(options);
-
-
